c8ebf85954325b03138bee51f5844ef1c693bbe9
vscardel <vscardel@gmail.com>
2021-07-22 19:51:29 -0300

    heroku com mysql

--- db.py
+++ db.py
@@ -4,8 +4,8 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
  
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'proagroFacil'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'b7a0d73d0e0c21'
+app.config['MYSQL_DATABASE_PASSWORD'] = '597fa7a7'
+app.config['MYSQL_DATABASE_DB'] = 'heroku_c48710de58032a8'
+app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-04.cleardb.com'
 mysql.init_app(app)
