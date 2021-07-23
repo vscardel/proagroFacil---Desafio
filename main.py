c04754d23c8d06a247b9fbfc7262ac59e72999c2
vscardel <vscardel@gmail.com>
2021-07-23 11:58:57 -0300

    tela de cadastro

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import csv
 import pymysql
 from app import app
 from db import mysql
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template,request,url_for
 
 conn = mysql.connect()
 cursor = conn.cursor()
@@ -11,6 +11,12 @@ cursor = conn.cursor()
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/cadastro',methods = ['GET','POST'])
+def formulario_cadastro():
+	if request.method == 'POST':
+		return redirect(url_for('index'))
+	return redirect(url_for('cadastro'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
