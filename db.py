3ce69eacfffc8353b097e4f07e92e72995430556
vscardel <vscardel@gmail.com>
2021-07-23 08:36:47 -0300

    novo banco

--- db.py
+++ db.py
@@ -4,8 +4,8 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
  
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'b7a0d73d0e0c21'
-app.config['MYSQL_DATABASE_PASSWORD'] = '597fa7a7'
-app.config['MYSQL_DATABASE_DB'] = 'heroku_c48710de58032a8'
+app.config['MYSQL_DATABASE_USER'] = 'b235ccdfe9b68f'
+app.config['MYSQL_DATABASE_PASSWORD'] = '2a032b91'
+app.config['MYSQL_DATABASE_DB'] = 'heroku_ca53e8cabd3c7ad'
 app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-04.cleardb.com'
 mysql.init_app(app)
