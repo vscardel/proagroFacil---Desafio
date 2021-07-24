729871687bd499df237abb504904c2a3c1ffabc2
vscardel <vscardel@gmail.com>
2021-07-24 13:09:11 -0300

    validando cpf

--- static/validation.js
+++ static/validation.js
@@ -1,3 +1,8 @@
+list_of_commom_valid_domains = ['gmail','hotmail','yahoo,','aol','msn',
+'mail','uol','ig''outlook'];
+
+list_of_commom_valid_tld = ['com','net','org','gov','edu','info','mil'];
+
 //checa se uma eh composta pelo mesmo caracter
 function check_same_char(my_str) {
 	let first_char = my_str[0];
@@ -55,13 +60,37 @@ function validate_cpf(e) {
 	}
 }
 
+//simplificações: checa por dominios e TLDs mais comuns.
+//se nenhum bater, retorna false. Não será verificado se
+//a parte local do endereço de e-mail é válida para evitar
+//o uso de regex e verificações muito complicadas
 function validade_email(e) {
 	email = document.getElementById('femail').value;
-	console.log(email);
+	let domain = "";
+	let flag_domain = false;
+	let tld = "";
+	let flag_tld = false;
+	for(let i=0;i<email.length; i++) {
+		if(email[i] == '@') {
+			flag_domain = true;
+		}
+		else if(email[i] == '.') {
+			flag_tld = true;
+			flag_domain = false;
+		}
+		if(flag_domain) {
+			domain += email[i];
+		}
+		if(flag_tld) {
+			tld += email[i];
+		}
+	}
+	console.log(domain);
+	console.log(tld);
 }
 
 function debug(e) {
 	e.preventDefault();
-	alert(validate_cpf());
+	validade_email();
 	return false;
 }
