f319d775c864cd05e3809630dcb7c22e03645a5d
vscardel <vscardel@gmail.com>
2021-07-24 11:39:41 -0300

    introduzindo js para validacao

--- static/validation.js
+++ static/validation.js
@@ -10,9 +10,8 @@ function check_same_char(my_str) {
 }
 
 function validate_cpf(e) {
-	console.log('oi');
 	let cpf = document.getElementById('fcpf');
-	console.log('oi2');
+	console.log(cpf);
 	// tamanho invalido
 	if(cpf.length != 11) {
 		return false;
