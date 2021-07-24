f282b428b7322947f43c3643ccb4e5b93782257d
vscardel <vscardel@gmail.com>
2021-07-24 11:34:37 -0300

    introduzindo js para validacao

--- static/validation.js
+++ static/validation.js
@@ -31,7 +31,7 @@ function validate_cpf(e) {
 			cont--;
 		}
 		let first_verification_result = (sum*10) % 11;
-		alert(first_verification_result);
+		console.log(first_verification_result);
 		if(first_verification_result != first_digit) {
 			return false;
 		}
@@ -43,7 +43,7 @@ function validate_cpf(e) {
 			cont--;
 		}
 		let second_verification_result = (sum*10) % 11;
-		alert(second_verification_result);
+		console.log(second_verification_result);
 		if(second_verification_result != second_digit) {
 			return false;
 		}
